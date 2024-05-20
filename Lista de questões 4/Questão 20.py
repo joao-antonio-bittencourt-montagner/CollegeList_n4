affd388f0b0c41ecc9f07b914fd02d5935e6add1
joao-antonio-bittencourt-montagner <joaoantoniob.montagner@gmail.com>
2024-05-20 16:41:03 -0400

    Questão 20.py

--- Lista de questões 4/Questão 20.py
+++ Lista de questões 4/Questão 20.py
@@ -1,11 +1,16 @@
 import random
-tupla = ()
-tupla1 = ()
-n1 = tuple(random.randint(0, 9) for i in range(10))
-n2 = tuple(random.randint(0, 9) for i in range(10))
-print(n1)
-print(n2)
-
-n3 = (set(n1) | set(n2))
-n3 = tuple(n3_)
-print(n3)
+d = 10
+c = 0
+l1 = [random.randint(0,15)for i in range(d)]
+l2 = [random.randint(0,15)for i in range(d)]
+l3 = l1 + l2
+l4 = []
+print(l1)
+print(l2)
+print()
+print(l3)
+for i in range(len(l3)):
+    if not l3[i] in l4:
+        l4.append(l3[i])
+print()
+print(sorted(l4))
